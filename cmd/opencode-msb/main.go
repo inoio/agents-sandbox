389498355682e771dce7d7d4561e3a5a97f057ec
Ole Langbehn <ole.langbehn@inoio.de>
2026-07-22 08:40:41 +0000

    refactor: switch cmd/ to new packages, delete old opencodemsb package

--- cmd/opencode-msb/main.go
+++ cmd/opencode-msb/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"gitlab.inoio.de/inoio/opencode-msb/internal/opencodemsb"
 )
 
 func main() {
-	if err := opencodemsb.Execute(); err != nil {
+	if err := Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
--- internal/opencodemsb/data.go
+++
@@ -1,9 +0,0 @@
-package opencodemsb
-
-import _ "embed"
-
-//go:embed data/Dockerfile
-var EmbeddedDockerfile []byte
-
-//go:embed data/provider-config.json
-var EmbeddedProviderConfig []byte
