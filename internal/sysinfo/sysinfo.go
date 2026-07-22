be6bd223c8a3cf991356c79349db088a45195676
Ole Langbehn <ole.langbehn@inoio.de>
2026-07-22 08:23:13 +0000

    refactor: create internal/sysinfo package

---
+++ internal/sysinfo/sysinfo.go
@@ -0,0 +1,49 @@
+package sysinfo
+
+import (
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+func NumCPUs() uint8 {
+	n := runtime.NumCPU()
+	if n < 1 {
+		n = 1
+	}
+	if n > 255 {
+		n = 255
+	}
+	return uint8(n)
+}
+
+func parseMemInfo(data []byte) (totalKB int, ok bool) {
+	for _, line := range strings.Split(string(data), "\n") {
+		if !strings.HasPrefix(line, "MemTotal:") {
+			continue
+		}
+		fields := strings.Fields(line)
+		if len(fields) < 2 {
+			return 0, false
+		}
+		kb, err := strconv.Atoi(fields[1])
+		if err != nil {
+			return 0, false
+		}
+		return kb, true
+	}
+	return 0, false
+}
+
+func TotalMemoryGiB() int {
+	data, err := os.ReadFile("/proc/meminfo")
+	if err != nil {
+		return 0
+	}
+	totalKB, ok := parseMemInfo(data)
+	if !ok {
+		return 0
+	}
+	return totalKB / (1024 * 1024)
+}
