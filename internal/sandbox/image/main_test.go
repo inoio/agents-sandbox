0b9f304a40e7f8c0f06a1e988bb354ef2321704c
Ole Langbehn <ole.langbehn@inoio.de>
2026-08-11 08:26:15 +0000

    refactor(sandbox): extract image module

---
+++ internal/sandbox/image/main_test.go
@@ -0,0 +1,18 @@
+package image
+
+import (
+	"testing"
+
+	"gitlab.inoio.de/inoio/opencode-msb/internal/configpaths"
+	"gitlab.inoio.de/inoio/opencode-msb/internal/sandbox/docker"
+	"gitlab.inoio.de/inoio/opencode-msb/internal/sandbox/msb"
+	"gitlab.inoio.de/inoio/opencode-msb/internal/testutil"
+)
+
+func TestMain(m *testing.M) {
+	testutil.InitMocks(m,
+		configpaths.InstallFailFastConfigPaths,
+		docker.InstallFailFastGet,
+		msb.InstallFailFastGet,
+	)
+}
